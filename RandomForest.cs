75b5699179f7661a5f3041e90b26e1fdadb7ab34
Илья <hunter12677@gmail.com>
2017-09-22 19:06:23 +0300

    Created class diagram

---
+++ RandomForest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RandomForest
+{
+    public class RandomForest
+    {
+        public RandomForest()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
