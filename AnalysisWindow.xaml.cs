f2d8bab4f2b5f9d9b69f63b0d17aabfe2c45e4bb
Nikita <snigirev.nikita.d@gmail.com>
2017-12-16 16:05:02 +0300

    Repair.

--- AnalysisWindow.xaml.cs
+++ AnalysisWindow.xaml.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Drawing;
+using System.IO;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
@@ -20,15 +22,195 @@ namespace RandomForest
     public partial class AnalysisWindow : Window
     {
         RForest rForest;
-        public AnalysisWindow(RForest rForestInstance)
+        SamplesContainer TrainingSamples;
+        SamplesContainer TestSamples;
+        public AnalysisWindow(RForest rForestInstance, SamplesContainer TrainingSamples, SamplesContainer TestSamples)
         {
             rForest = rForestInstance;
+            this.TrainingSamples = TrainingSamples;
+            this.TestSamples = TestSamples;
             InitializeComponent();
+            DrawTree(0);
+
+            List<int> numbersList = new List<int>();
+            for (int i = 0; i < rForestInstance.trees.Count; i++)
+                numbersList.Add(i);
+            treeComboBox.ItemsSource = numbersList;
+            treeComboBox.SelectedIndex = 0;
+
+            samplesComboBox.ItemsSource = TestSamples.samplesList.Select(p => p.SampleName).ToList();
+            samplesComboBox.SelectedIndex = 0;
+
+            classComboBox.ItemsSource = TrainingSamples.classLabels;
+            classComboBox.SelectedIndex = 0;
+
+            DrawComparsion(TestSamples.samplesList[samplesComboBox.SelectedIndex]);
         }
 
         public void DrawTree(int index)
         {
             Node tree = (Node)rForest.trees[index];
+            //collection of information about a tree
+            List<int> offsLst = new List<int>();
+            List<int> posLst = new List<int>();
+            treeInfo(tree, 0, offsLst);
+            int treeHeight = offsLst.Count;
+            int treeWidth = offsLst.Max();
+            string treeStr="";
+            treeStr += "Tree width: " + treeWidth +"\n" + "Tree height: " + treeHeight +"\n" +"Power of the tree: "+offsLst.Sum();
+            aboutTreTextBox.Text = treeStr;
+
+            Bitmap treeBitMap = new Bitmap(treeWidth * 180, treeHeight * 100);
+            Graphics g = Graphics.FromImage(treeBitMap);
+            g.FillRegion(new SolidBrush(System.Drawing.Color.AntiqueWhite), new Region(new System.Drawing.Rectangle(0, 0, treeWidth * 180, treeHeight * 100)));
+            //tree drawing
+            treeOutput(g, tree, posLst, 0, 0, 0, offsLst, treeWidth, "");
+            g.Dispose();
+
+
+            treeImage.Source = BitmapToImageSource(treeBitMap);
+        }
+
+        void DrawComparsion(Sample sample)
+        {
+            List<string> treesDecisions = new List<string>();
+            foreach (DecisionTree tree in rForest.trees)
+            {
+                treesDecisions.Add(tree.Decide(sample));
+            }
+            
+            Bitmap graphBitMap = new Bitmap(300,100);
+            Graphics g = Graphics.FromImage(graphBitMap);
+            g.FillRegion(new SolidBrush(System.Drawing.Color.WhiteSmoke), new Region(new System.Drawing.Rectangle(0, 0, 300,100)));
+
+            SolidBrush myBrush = new SolidBrush(System.Drawing.Color.Red);
+
+            int width = graphBitMap.Width / treesDecisions.Count;
+            for(int i = 1;i<=treesDecisions.Count;i++)
+                {
+                    float percent = (float)(treesDecisions.Take(i).Where(p => p == (string)classComboBox.SelectedItem).ToList().Count) / (float)(treesDecisions.Count);
+
+                    g.FillRectangle(myBrush, (i-1)*width, graphBitMap.Height - (float)(graphBitMap.Height)*(float)(percent), i * width, (float)graphBitMap.Height * (float)percent);
+                }
+
+            myBrush.Dispose();
+            g.Dispose();
+            graphImage.Source = BitmapToImageSource(graphBitMap);
+        }
+
+        void treeInfo(DecisionTree node, int level, List<int> curLst)
+        {
+            if (curLst.Count == level) curLst.Add(0);
+            if (node.GetType() == typeof(Node))
+            {
+                curLst[level]++;
+                foreach (DecisionTree branch in ((Node)node).branches.Values)
+                {
+                    treeInfo(branch, level + 1, curLst);
+                }
+            }
+            else
+            {
+                curLst[level]++;
+            }
+        }
+
+        void treeOutput(Graphics g, DecisionTree node, List<int> posLst, int level, int preX, int preY, List<int> offsLst, int maxWidth, string atrVal)
+        {
+            System.Drawing.Pen myPen = new System.Drawing.Pen(System.Drawing.Color.Black);
+            System.Drawing.Font nodeFont = new System.Drawing.Font("Courier", 12, System.Drawing.FontStyle.Regular);
+            System.Drawing.Font branchFont = new System.Drawing.Font("Courier", 12, System.Drawing.FontStyle.Regular);
+            System.Drawing.SolidBrush drawBrush = new System.Drawing.SolidBrush(System.Drawing.Color.Black);
+            if (posLst.Count == level) posLst.Add(0);
+            int curX = posLst[level];
+
+            int paddingY = 75;
+            int dx = 120;
+            int dy = 25;
+
+            int x = 5 + (curX + (maxWidth - offsLst[level]) / 2) * dx + 60 * curX;
+            int y = 5 + (int)(level * dy + level * paddingY);
+            int preXV = 0;
+            if (level > 0) preXV = 5 + (preX + (maxWidth - offsLst[level - 1]) / 2) * dx + 60 * preX;
+            int preYV = 5 + (int)(preY * dy + preY * paddingY) + dy;
+
+            if (node.GetType() == typeof(Node))
+            {
+                posLst[level]++;
+                g.DrawRectangle(myPen, new System.Drawing.Rectangle(x, y, dx, dy));
+
+                g.DrawString(((Node)node).attribute, nodeFont, drawBrush, x, y);
+                g.DrawString(atrVal, branchFont, drawBrush, x, y - dy);
+                if (preX != curX || preY != level)
+                {
+                    g.DrawLine(myPen, x + 60, y, preXV + 60, preYV);
+
+                }
+
+                drawBrush.Dispose();
+                branchFont.Dispose();
+                nodeFont.Dispose();
+                myPen.Dispose();
+
+                foreach (KeyValuePair<string, DecisionTree> branch in ((Node)node).branches)
+                {
+                    treeOutput(g, branch.Value, posLst, level + 1, curX, level, offsLst, maxWidth, branch.Key);
+                }
+
+            }
+            else
+            {
+
+                g.DrawLine(myPen, x + 60, y, preXV + 60, preYV);
+                myPen.Color = System.Drawing.Color.Green;
+                g.DrawRectangle(myPen, new System.Drawing.Rectangle(x, y, dx, dy));
+                g.DrawString(atrVal, branchFont, drawBrush, x, y - dy);
+                drawBrush.Color = System.Drawing.Color.Red;
+                g.DrawString(((Leaf)node).classLabel, nodeFont, drawBrush, x, y);
+                posLst[level]++;
+
+                drawBrush.Dispose();
+                branchFont.Dispose();
+                nodeFont.Dispose();
+                myPen.Dispose();
+            }
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            this.Owner.IsEnabled = true;
+            this.Owner.Show();
+        }
+
+        BitmapImage BitmapToImageSource(Bitmap bitmap)
+        {
+            using (MemoryStream memory = new MemoryStream())
+            {
+                bitmap.Save(memory, System.Drawing.Imaging.ImageFormat.Bmp);
+                memory.Position = 0;
+                BitmapImage bitmapimage = new BitmapImage();
+                bitmapimage.BeginInit();
+                bitmapimage.StreamSource = memory;
+                bitmapimage.CacheOption = BitmapCacheOption.OnLoad;
+                bitmapimage.EndInit();
+
+                return bitmapimage;
+            }
+        }
+
+        private void treeComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            DrawTree(treeComboBox.SelectedIndex);
+        }
+
+        private void samplesComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            DrawComparsion(TestSamples.samplesList[samplesComboBox.SelectedIndex]);
+        }
+
+        private void classComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            DrawComparsion(TestSamples.samplesList[samplesComboBox.SelectedIndex]);
         }
     }
 }
